d94c4142bded319d99ae37a81d72318f085228a8
RAJKIRAN S <pravinraj0073@gmail.com>
2025-09-07 16:18:36 +0530

    Add files via upload

---
+++ raj 2.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("I Study in KAHE");
+    return 0;
+}
+
---
+++ raj5.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>>
+int main()
+{
+	float num=25.234;
+	printf("%f",num);
+	return 0;
+}
